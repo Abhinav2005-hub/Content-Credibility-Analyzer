0178e39458553d3b93ad39853ceaa77f28800b2d
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-09-26 23:50:33 +0530

    verify RAG pipeline

--- server/src/services/test-rag-verification.js
+++ server/src/services/test-rag-verification.js
@@ -1,5 +1,5 @@
 import { runRagVerification } from "./ragVerification.service.js";
 
-const result = await runRagVerification(1, 2);
+const result = await runRagVerification(4, 4);
 
 console.log(JSON.stringify(result, null, 2));
